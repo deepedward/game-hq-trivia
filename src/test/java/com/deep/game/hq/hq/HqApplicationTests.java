43074e3be373974d33eb4d55013c413cf6b312c3
deepakedward <deepakedward18@gmail.com>
2020-07-07 22:53:32 -0700

    initial commit

---
+++ src/test/java/com/deep/game/hq/hq/HqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deep.game.hq.hq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
